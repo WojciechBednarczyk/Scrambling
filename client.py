8ef33e665f5d6788c4e9293f91a271efee0675a3
wojkacz <252813@student.pwr.edu.pl>
2021-04-21 18:59:18 +0200

    komentarze, wyciąganie pojedynczych bitów, poprawa przesyłania, zliczanie takich samych bitów z rzędu

--- client.py
+++ client.py
@@ -1,24 +1,28 @@
 import socket
 
+# Wczytywanie docelowego adresu IP od klienta
 HostIP = input("Type IP (leave blank for 127.0.0.1): ")
 if not HostIP:
     HostIP = '127.0.0.1'
-    
+
+# Port do połączenia
 HostPort = 5555
 
+# Wczytywanie nazwy pliku, do którego chcemy zapisać odebrane dane, otwieranie pliku
 name = input("Type file name to save: ")
 file = open(name, "wb")
 
+# Łączenie z serwerem
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HostIP, HostPort))
 
 while True:
-    received = client_socket.recv(1024)
-    if str(received) == "b''":
+    received = client_socket.recv(1024) # Odbieranie 1024 bajtów danych od serwera
+    if str(received) == "b''":          # Jeśli odebrano pusty bajt wyjdź z pętli
         break
-    file.write(received)
+    file.write(received)                # Zapisz odebrane dane do pliku
 
 print(f"File was saved.")
-file.close()
-client_socket.close()
+file.close()                            # Zamykanie pliku
+client_socket.close()                   # Zamykanie socketa klienta
 
--- server.py
+++ server.py
@@ -1,25 +1,55 @@
 import socket
+import numpy
 
+# Wczytywanie adresu IP serwera
 HostIP = input("Type IP (leave blank for 127.0.0.1): ")
 if not HostIP:
     HostIP = '127.0.0.1'
-    
+
+# Port serwera
 HostPort = 5555
 
+# Tworzenie serwera
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HostIP, HostPort))
 server_socket.listen(5)
 
+# Wczytywanie nazwy pliku do wysłania
 image = input("Type file name: ")
 
+# Nasłuchiwanie połączenia
 print(f"Server started, listening for connection.")
-
 client_socket, client_address = server_socket.accept()
+
+# Wyświetlanie informacji o przychodzącym połączeniu
 print(f"Connection from {client_address}, sending file...")
 
+# Otwieranie pliku i wysyłanie
 imgFile = open(image, 'rb')
-for i in imgFile:
-    client_socket.send(i)
 
-imgFile.close()
+# Rozpakowywanie bajtów na bity
+Bytes = numpy.fromfile(image, dtype = "uint8")
+Bits = numpy.unpackbits(Bytes)
+
+amount = 0          # Ilosc takich samych bitów z kolei (kazde jedno to 1% szans na pominięcie)
+isZeroNow = True    # Uzywane do zliczania ilości takich samych bitów z kolei
+
+for j in Bits:      # Dla kazdego bitu, j -> bit
+    if (j == 0 and isZeroNow) or (j == 1 and not isZeroNow):    # Zliczanie
+        if amount < 100:
+            amount += 1
+    else:
+        isZeroNow = not isZeroNow
+        amount = 0
+    # !! Tutaj trzeba wkleic coś zeby według procentów skipowało bit
+
+# Pakowanie bitów z powrotem w bajty i wysyłanie
+Bytes = numpy.packbits(Bits)
+client_socket.send(Bytes)
+
+# !! Tu trzeba jakiegoś ifa dać czy faktycznie plik się wysłał
 print(f"[To {client_address}] File sent!")
+
+# Zamykanie połączenia
+imgFile.close()
+server_socket.close()
