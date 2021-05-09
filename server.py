546d78208d2316293b9c931f8980ac51b6eb77f6
Łukasz <mieczynskiukasz@gmail.com>
2021-05-10 00:57:30 +0200

    działa tylko dla image.bmp, jest on w rozmiarze 8x4 piksele, bity od 488 są odpowiedzialne za kolor obrazu

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ import numpy
 import random
 
 # Wczytywanie adresu IP serwera
+import numpy as np
+
 HostIP = input("Type IP (leave blank for 127.0.0.1): ")
 if not HostIP:
     HostIP = '127.0.0.1'
@@ -31,20 +33,15 @@ imgFile = open(image, 'rb')
 # Rozpakowywanie bajtów na bity
 Bytes = numpy.fromfile(image, dtype="uint8")
 Bits = list(numpy.asarray(numpy.unpackbits(Bytes))) # Bity jako lista
-# Bits = numpy.unpackbits(Bytes)
 print(Bytes)
 #zmiana w tablicy od 500 indeksu do końca
-Bytes[500:] = 255
 
-
-
-print(Bytes)
 amount = 0          # Ilosc takich samych bitów z kolei (kazde jedno to 0.1% szans na pominięcie)
 isZeroNow = True    # Uzywane do zliczania ilości takich samych bitów z kolei
-index = 0           # Indeks pętli, nie miałem pomysłu jak to sensownie zrobić xd
-
-for j in Bits:      # Dla kazdego bitu, j -> bit
-    if (j == 0 and isZeroNow) or (j == 1 and not isZeroNow):    # Zliczanie
+index = 0     # Indeks pętli, nie miałem pomysłu jak to sensownie zrobić xd
+j=488
+while j< len(Bits):      # Dla kazdego bitu, j -> bit
+    if (Bits[j] == 0 and isZeroNow) or (Bits[j]  == 1 and not isZeroNow):    # Zliczanie
         if amount < 100:
             amount += 0.1
     else:
@@ -52,19 +49,21 @@ for j in Bits:      # Dla kazdego bitu, j -> bit
         amount = 0
 
     # Skipuje bit jak wylosuje się odpowiednia liczba
-#    rand = random.randint(1, 100)
-#    if rand <= amount:
-#        print(f"[Debug] Usuwanie bitu [Bit: {j}] [Losowa: {rand}] [Procent: {amount}], [Index: {index}]")
-#        Bits.pop(index)
-#        amount = 0
+    rand = random.randint(1, 100)
+    if rand <= amount:
+        print(f"[Debug] Usuwanie bitu [Bit: {Bits[j]}] [Losowa: {rand}] [Procent: {amount}], [Index: {j}]")
+        Bits.pop(j)     #usuwanie bitu na pozycji  j
+        Bits.append(1); #dodawanie 1 na koniec
+        amount = 0
 
 # Ogolnie to problem jest z tym pomijaniem bitów bo jak pomija chociazby 1 bit to plik nie działa
 # Próbowałem kilka wersji tego i testowałem i nie wiem jak to rozwiązać, moze wam się uda
-    index += 1
+    j += 1
 
 
 # Pakowanie bitów z powrotem w bajty i wysyłanie
-#Bytes = numpy.packbits(Bits)
+Bytes = np.packbits(Bits, axis=-1)
+print(Bytes)
 client_socket.send(Bytes)
 
 # !! Tu trzeba jakiegoś ifa dać czy faktycznie plik się wysłał
