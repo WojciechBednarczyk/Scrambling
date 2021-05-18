7c205222d87bff4a2239a5fe14db200e98bef9ca
wojkacz <252813@student.pwr.edu.pl>
2021-05-18 19:24:44 +0200

    Przepuszczanie przez scramblery i zapisywanie

--- server.py
+++ server.py
@@ -6,9 +6,9 @@ import random
 import cv2
 import time
 #liczniki bitów dla poszczególnych scramblerów
-counter_dvb = [0,0]
-counter_v34 = [0,0]
-counter_x16 = [0,0]
+counter_dvb = [0, 0]
+counter_v34 = [0, 0]
+counter_x16 = [0, 0]
 
 data_counter = [0,0]
 # ================================ Zegary
@@ -105,74 +105,68 @@ def sumOfBits(signal):
         else:
             data_counter[1] += 1
 
-
-image_data_bits = []
-
 # Zamiana bajtow obrazu na bity
-def image_to_bits(image_data):
-    for i in range(0, len(image_data)):
+def image_to_bits(data, bits):  # data -> bajty, bits -> docelowe zapisanie bitów
+    for i in range(0, len(data)):
         # Konwersja bajtu na bity
-        current_byte = format(image_data[i], '08b')
+        current_byte = format(data[i], '08b')
 
         bit_array = []
         for j in range(0, len(current_byte)):
             bit_array.append(int(current_byte[j]))
         # Dodawanie bitow badanego bajtu do tablicy bitow
         for k in range(0, len(bit_array)):
-            image_data_bits.append(bit_array[k])
+            bits.append(bit_array[k])
 
 
-def switch_bits():
+def switch_bits(bits):
     amount = 0         # Ilosc takich samych bitów z kolei (kazde jedno to 0.1% szans na pominięcie)
     isZeroNow = True   # Uzywane do zliczania ilości takich samych bitów z kolei
     index = 0          # Indeks pętli
 
-    while index < len(image_data_bits):  # Dla kazdego bitu, j -> bit
-        if (image_data_bits[index] == 0 and isZeroNow) or (image_data_bits[index] == 1 and not isZeroNow):  # Zliczanie
+    while index < len(bits):  # Dla kazdego bitu, j -> bit
+        if (bits[index] == 0 and isZeroNow) or (bits[index] == 1 and not isZeroNow):  # Zliczanie
             if amount < 100:
                 amount += 0.25
         else:
             isZeroNow = not isZeroNow
-            # zakomentowalem wedlug jego zalecen zeby nie zerowac tej szansy
-            # ^ ale ta szansa się resetuje jak znajdzie przeciwny bit, wtedy od zera właśnie powinno liczyć szanse
-            # to o czym mówił to to, zeby nie zerować po usunięciu/zamienieniu bitu, usunąłem to
             amount = 0
 
         # Skipuje bit jak wylosuje się odpowiednia liczba
         rand = random.randint(1, 100)
         if rand <= amount:
-            print(
-                f"[Debug] Zamienianie bitu [Bit: {image_data_bits[index]}] [Losowa: {rand}] [Procent: {amount}], [Index: {index}]")
-            if image_data_bits[index] == 0:
-                image_data_bits.pop(index)
-                image_data_bits.insert(index, 1)
+            #print(f"[Debug] Zamienianie bitu [Bit: {bits[index]}] [Losowa: {rand}] [Procent: {amount}], [Index: {index}]")
+            if bits[index] == 0:
+                bits.pop(index)
+                bits.insert(index, 1)
             else:
-                image_data_bits.pop(index)
-                image_data_bits.insert(index, 0)
+                bits.pop(index)
+                bits.insert(index, 0)
         index += 1
 
 
-def bits_to_bytes():    # Zamiana z powrotem na bajty
+def bits_to_bytes(bits, array):    # Zamiana z powrotem na bajty
     bit_holder = []
     bit_counter = 0
-    data_byte_counter = 0
-    for i in range(0, len(image_data_bits)):
-        bit_holder.append(image_data_bits[i])
+    data = []
+    #data_byte_counter = 0
+    for i in range(0, len(bits)):
+        bit_holder.append(bits[i])
         bit_counter += 1
         if bit_counter == 8:
             string = ''
-            for i in range(0, len(bit_holder)):
-                string += str(bit_holder[i])
-            image_data[data_byte_counter] = int(string, 2)
+            for j in range(0, len(bit_holder)):
+                string += str(bit_holder[j])
+            data.append(int(string, 2))
             bit_counter = 0
-            data_byte_counter += 1
+            #data_byte_counter += 1
             bit_holder = []
 
     byte_counter = 0
-    for i in range(0, len(image_array)):
-        for j in range(0, len(image_array[i])):
-            for z in range(0, len(image_array[i][j])):
-                image_array[i][j][z] = image_data[byte_counter]
+    for i in range(0, len(array)):
+        for j in range(0, len(array[i])):
+            for k in range(0, len(array[i][j])):
+                array[i][j][k] = data[byte_counter]
                 byte_counter += 1
 
 
@@ -186,12 +180,36 @@ for x in range(0, len(image_array)):
         for z in range(0, len(image_array[x][y])):
             image_data.append(image_array[x][y][z])
 
-image_to_bits(image_data)
-switch_bits()
-bits_to_bytes()
+# Zamiana obrazka na bity
+image_data_bits = []        # Bity początkowego obrazka
+image_to_bits(image_data, image_data_bits)
 
-#zapis obrazu
-cv2.imwrite('wynik.jpg', image_array)
+# Kopiowanie obrazka i zamiana bitów w kopii
+image_switched_bits = image_data_bits.copy()
+switch_bits(image_switched_bits)
+
+# Zamiana na bajty i zapisywanie
+image_switched_array = image_array.copy()
+bits_to_bytes(image_switched_bits, image_switched_array)
+cv2.imwrite('switched_bits.jpg', image_switched_array)
+
+# Scramblowanie DVB
+scrambled_dvb_bits = scramDVB(image_data_bits.copy())
+scrambled_dvb_array = image_array.copy()
+bits_to_bytes(scrambled_dvb_bits, scrambled_dvb_array)
+cv2.imwrite('DVB_scrambled.jpg', scrambled_dvb_array)
+
+# Scramblowanie V34
+scrambled_v34_bits = scramV34(image_data_bits.copy())
+scrambled_v34_array = image_array.copy()
+bits_to_bytes(scrambled_v34_bits, scrambled_v34_array)
+cv2.imwrite('V34_scrambled.jpg', scrambled_v34_array)
+
+# Scramblowanie X16
+scrambled_x16_bits = scramX16(image_data_bits.copy())
+scrambled_x16_array = image_array.copy()
+bits_to_bytes(scrambled_x16_bits, scrambled_x16_array)
+cv2.imwrite('X16_scrambled.jpg', scrambled_x16_array)
 
 
 
