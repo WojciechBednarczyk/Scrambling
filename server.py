757f0856a494e2ad10df66696e969fd93d215f8d
Łukasz <mieczynskiukasz@gmail.com>
2021-05-18 17:43:27 +0200

    scramblery i zegary

--- server.py
+++ server.py
@@ -1,7 +1,110 @@
+from operator import xor
+
+import numpy
 import numpy as np
 import random
 import cv2
 import time
+#liczniki bitów dla poszczególnych scramblerów
+counter_dvb = [0,0]
+counter_v34 = [0,0]
+counter_x16 = [0,0]
+
+data_counter = [0,0]
+# ================================ Zegary
+# Metoda zegara dla scramblerów addytywnych, sprzężenie zwrotne xora dla bitów ramki i sygnału wejściowego
+def sync_clock(frame, data, bit, counter):
+    if bit[1]!=-1:                                     # Sprawdzanie czy używamy obu bitów, potrzebne dla niektórych scramblerów
+        temp = xor(frame[bit[0]-1], frame[bit[1]-1])     #  XOR dla bit[0] i bit[1],
+    else:                                              # jeśli tylko 1 bit, przypisujemy wartość temu bitowi
+        temp = frame[bit[0]-1]
+    frame.pop()                                         # usuwanie ostatniego bitu z ramki
+    frame.insert(0,temp)                            # dodanie na początek wartości xor
+    if temp == 1:                                  #zliczanie 0 i 1, z operacji xor
+        counter[1] +=1
+    else:
+        counter[0]  +=1
+    xor_value = xor(temp, data)                      # sprzężenie zwrotne wartości syganłu wejściowego i xora z bitów ramki
+    return xor_value                                     # zwrócenie rezultatu
+#Metoda zegara dla scramblerów multiplikatywnych, sprzężenie zwrotne xora dla bitów ramki i sygnału wejściowego
+def async_clock(frame, data, bit, counter):
+    if bit[1]!=-1:                                     # Sprawdzanie czy używamy obu bitów, potrzebne dla niektórych scramblerów
+      temp = xor(frame[bit[0]-1], frame[bit[1]-1])   # XOR dla bit[0] i bit[1],
+    else:                                              # jeśli tylko 1 bit, przypisujemy wartość temu bitowi
+        temp = frame[bit[0]-1]
+    frame.pop()                                         # usuwanie ostatniego bitu z ramki
+    xor_value = xor(temp, data)                      # sprzężenie zwrotne wartości syganłu wejściowego i xora z bitów ramki
+    if xor_value==1:                                     #zliczanie 0 i 1, z operacji xor
+        counter[1] +=1
+    else:
+        counter[0] +=1
+    frame.insert(0,xor_value)                             # dodawanie na początek ramki xora
+    return xor_value                                     # rezultat zakodowanego sygnału
+
+# Metoda zegara dla desramblerów multiplikatywnych
+def reverse_async_clock(frame, data, bit):
+    if bit[1]!=-1:                                     # Sprawdzanie czy używamy obu bitów, potrzebne dla niektórych scramblerów
+        temp = xor(frame[bit[0]-1], frame[bit[1]-1]) # XOR dla bit[0] i bit[1],
+    else:                                              # jeśli tylko 1 bit, przypisujemy wartość temu bitowi
+        temp = frame[bit[0]-1]
+    frame.pop()                                         # Usuwanie ostatniego elementu ramki
+    frame.insert(0,data)                                # dodawanie na początek ramki bitu sygnału
+    xor_value = xor(data, temp)                      # XOR dla bitu synganłu wejściowego i poprzedniego xora
+    return xor_value                                     # Zwrócenie zdekodowanego sygnału
+# ===============SCRAMBLERS================= SCRAMBLERS
+
+# DVB Scrambler addytywny
+def scramDVB(signal):
+    dataLength = len(signal)                                     # Długość sygnału wejściowego
+    frameDVBS=[1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0]       # Ramka  synchronizująca dla scramblera
+    scramBit=[len(frameDVBS),len(frameDVBS)-1]                     # bity używane przy sprzężeniu zwrotnym - dla DVB jest to ostatni i przedostatni bit
+    output_signal = []                                                  # tablica na dane wyjściowe
+    for i in range(0,dataLength):                                # iteracja po całej tablicy danych wejściowych
+        clock_result = sync_clock(frameDVBS,signal[i], scramBit, counter_dvb)  # wykonanie operacji zegara dla sclamblera addytywnego
+        output_signal.append(clock_result)                              # dodanie wyników do tablicy danych wyjściowych
+    return output_signal
+
+# V34 Scrambler multiplikatywny
+def scramV34(signal):
+    dataLength = len(signal)
+    frameV34 = [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1] #ramka
+    scramBits = [18,23]                                          # Bity używane w sprzężeniu zwrotym, dla V34 bit 18 i 23
+    output_signal = []                                                  # tablica na dane wyjściowe
+    for i in range(0,dataLength):                                # iteracja po całej tablicy danych wejściowych
+        clock_result = async_clock(frameV34, signal[i],scramBits, counter_v34) # wykonanie operacji zegara
+        output_signal.append(clock_result)                              # dodanie wyników do tablicy danych wyjściowych
+    return output_signal
+
+# V34 Desrambler multiplikatywny
+def descramV34(signal):
+    dataLength = len(signal)
+    frameV34 = [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1]  # ramka
+    scramBits = [18, 23]                                                             # Bity używane w sprzężeniu zwrotym, dla V34 bit 18 i 23
+    output_signal = []                                                                      # tablica na dane wyjściowe
+    for i in range(0,dataLength):
+        clock_result = reverse_async_clock(frameV34,signal[i],scramBits) # operacja operacji dekodowania
+        output_signal.append(clock_result)                                       # dodanie wyników do tablicy danych wyjściowych
+    return output_signal
+
+#  x^16+1 Scrambler addytywny
+def scramX16(signal):
+    dataLength = len(signal)
+    frameX16 = [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1] # ramka
+    scramBit=[16,-1]                                              # Bity używane w sprzężeniu zwrotym, dla x16 bit 16, -1 dla braku drugiego bitu
+    output_signal = []                                                   # tablica na dane wyjściowe
+    for i in range(0,dataLength):
+        clock_result = sync_clock(frameX16,signal[i], scramBit, counter_x16)    # operacja zegara
+        output_signal.append(clock_result)                                             # dodanie wyników do tablicy danych wyjściowych
+    return output_signal
+
+
+def sumOfBits(signal):
+    for i in range(0, len(signal)):
+        if signal[i] == 0:
+            data_counter[0] += 1
+        else:
+            data_counter[1] += 1
+
 
 image_data_bits = []
 
@@ -89,3 +192,17 @@ bits_to_bytes()
 
 #zapis obrazu
 cv2.imwrite('wynik.jpg', image_array)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
